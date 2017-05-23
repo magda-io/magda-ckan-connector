af6702f0c087e5aa8c0256d24c302fee192a9f0d
Kevin Ring <kevin@kotachrome.com>
2017-05-23 15:23:09 +1000

    Add Project Open Data (data.json) connector.

--- src/AspectBuilder.ts
+++
@@ -1,9 +0,0 @@
-import { AspectDefinition } from './generated/registry/api';
-
-interface AspectBuilder {
-    aspectDefinition: AspectDefinition,
-    builderFunctionString: string,
-    setupFunctionString?: string
-}
-
-export default AspectBuilder;
